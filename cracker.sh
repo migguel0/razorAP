40892da9acb8764582aaff0d0ebaff16a7cf2754
migguel0 <43185771+migguel0@users.noreply.github.com>
2020-02-13 16:44:18 -0500

    Update cracker.sh

--- cracker.sh
+++ cracker.sh
@@ -1,51 +1,100 @@
 #!/bin/bash
 
-johnTool="john"
-asleapTool="asleap"
+#------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------
+#REVISAR EL JOHN VA RARO
+function johnCracking(){
+    	echo -e "\e[33mHas seleccionado John the Reaper\e[32m"
 
-cat hostapd-wpe.log | sed 's/::::/ /g' > logaux.log
-
-
-awk '/hashcat/ {print $4}' logaux.log > soloHash.txt
-
-echo -e "\e[33mSeleccione la herramienta para la fase de cracking: [john] [response]\e[39m"
-read tool
-
-
-if [ $tool = $johnTool ]; then
-	echo -e "\e[33mHas seleccionado John the Reaper\e[32m"
+	echo -e "\e[33mIndique la ruta absoluta del diccionarioa utilizar\e[39m"
+	read diccionario
 	#------------------------------------------------------------------------------------------
 	#------------------------------------------------------------------------------------------
 	#Mostrar lineas con el HASH y que decida el usuario el numero de linea que quiere crackear
-	awk '/hashcat/ {print $3}' hostapd-wpe.log | sed '=' | sed 's/::::/ /g'
+	awk '/jtr/ {print $3}' hostapd-wpe.log > /var/tmp/soloHash.txt
+	sed '=' /var/tmp/soloHash.txt
 
 	echo -e "\e[33mIndique el numero de HASH que quiere crackear\e[39m"
 	read lineNumber
-	awk "NR==$lineNumber {print \$1}" soloHash.txt
-	#------------------------------------------------------------------------------------------
-	#------------------------------------------------------------------------------------------
-elif [ $tool = $asleapTool ]; then
+	awk "NR==$lineNumber {print \$1}" /var/tmp/soloHash.txt
+	awk "NR==$lineNumber {print \$1}" /var/tmp/soloHash.txt > /var/tmp/hashSelected.hash
+	john --show $diccionario /var/tmp/hashSelected.hash
+	rm /var/tmp/hashSelected.hash
+}
+
+
+function asleapCracking(){
+	counter=0
 	echo -e "\e[33mHas seleccionado asleap\e[39m"
+
+	echo -e "\e[33mIndique la ruta absoluta del diccionarioa utilizar\e[39m"
+	read diccionario
 	#------------------------------------------------------------------------------------------
 	#------------------------------------------------------------------------------------------
 	#Mostrar lineas con el RESPONSE y que decida el usuario el numero de linea que quiere crackear
-	awk '/username/ {print $2}' logaux.log > users.txt
-	awk '/response/ {print $2}' logaux.log > response.txt
+	
+	awk '/username/ {print $2}' logaux.log > /var/tmp/users.txt
+	awk '/response/ {print $2}' logaux.log > /var/tmp/response.txt
+	awk '/challenge/ {print $2}' logaux.log > /var/tmp/challenge.txt
 	>parser.txt
 	while read -a linea
 	do
 		counter=$((counter + 1))
-		OUTPUT=$(awk "NR==$counter {print \$1}" aux.txt)
-		echo "$linea response: $OUTPUT " >> parser.txt
-	done < users.txt
-
-	#Borrar archivo de log auxiliar
-	rm logaux.log
+		OUTPUT=$(awk "NR==$counter {print \$1}" /var/tmp/response.txt)
+		CHALLENGE=$(awk "NR==$counter {print \$1}" /var/tmp/challenge.txt)
+		echo "$linea challenge: $CHALLENGE response: $OUTPUT " >> parser.txt
+	done < /var/tmp/users.txt
+	
 	awk '/response/' parser.txt | sed '='
 
 	echo -e "\e[33mIndique el numero de HASH que quiere crackear\e[39m"
 	read lineNumber
-	awk "NR==$lineNumber {print \$3}" parser.txt
+	#awk "NR==$lineNumber {print \$3}" parser.txt
+	challenge=$(awk "NR==$lineNumber {print \$3}" parser.txt)
+	response=$(awk "NR==$lineNumber {print \$5}" parser.txt)
+	rm /var/tmp/users.txt
+	rm /var/tmp/response.txt
+	rm /var/tmp/challenge.txt
+	rm parser.txt
+
+	asleap -C $challenge -R $response -W $diccionario
 	#------------------------------------------------------------------------------------------
 	#------------------------------------------------------------------------------------------
-fi
+}
+
+#------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------
+
+johnTool="john"
+asleapTool="asleap"
+no="no"
+
+cat hostapd-wpe.log | sed 's/::::/ /g' > logaux.log
+
+
+
+
+#Bucle infinito hasta que diga NO
+while :
+do
+	echo -e "\e[33mSeleccione la herramienta para la fase de cracking: [john] [asleap]\e[39m"
+	read tool
+
+	if [ $tool = $johnTool ]; then
+		johnCracking
+	elif [ $tool = $asleapTool ]; then
+		asleapCracking
+	fi
+	
+	echo -e "\e[33m¿Desea probar con otro Hash u otra herramienta de nuevo? [si] [no]\e[39m"
+	read anotherLoop
+	if [ $anotherLoop = $no ]; then
+		#Borrar archivo de log auxiliar
+		rm logaux.log
+		break
+	fi
+done
+
+
+
+
