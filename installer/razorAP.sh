59aa41d71fa22ef26072dbc530d8cae996796637
migguel04 <43185771+migguel0@users.noreply.github.com>
2020-09-16 15:17:46 -0400

    fixing errors

--- installer/razorAP.sh
+++ installer/razorAP.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+touch hostapd-wpe.log
+
+echo -e "\e[32m ------------------------------------------ \e[39m"
+echo -e "\e[32m ------------------------------------------ \e[39m"
+echo -e "\e[32m ------- RAZOR AP CONFIGURATION ----------- \e[39m"
+echo -e "\e[32m ------------------------------------------ \e[39m"
+echo -e "\e[32m ------------------------------------------ \e[39m"
+echo
+echo
+echo
+
 pwd=$(pwd)
 
 	#echo "Error: Primer parametro del script NOMBRE_CERTIFICADO, segundo parametro SSID y tercer parametro interfaz a usar y cuarto parametro CommonName."
@@ -43,13 +54,13 @@ echo -e "\e[33mElaborando certificados y archivos de configuracion... \e[39m"
 python $pwd/hostapdWPEinstall/apd_launchpad/apd_launchpad.py -t $cert -s $SSID -i $interface -cn $commonName
 
 if [ $? = 0 ]; then
-	echo '\e[33mTodo generado correctamente'
-	echo '\e[33mLevantando hosatpd-wpe'
-	echo "\e[33mSe va a ejecutar: hostapd-wpe $pwd/$cert/$cert.conf -s"
+	echo -e "\e[33mTodo generado correctamente \e[39m"
+	echo -e "\e[33mLevantando hosatpd-wpe \e[39m"
+	echo -e "\e[33mSe va a ejecutar: hostapd-wpe $pwd/$cert/$cert.conf -s \e[39m"
 	echo
 
 	echo "" >> $pwd/$cert/$cert.conf
-	echo "wpe_logfile=$pwd/hostapd-wpe.log" >> $pwd/$cert/$cert.conf
+	echo "#wpe_logfile=$pwd/hostapd-wpe.log" >> $pwd/$cert/$cert.conf
 	#configuracion extra para 5GHz
 	if [$band != $YES]; then
 		echo "hw_mode=a" >> $pwd/$cert/$cert.conf
@@ -63,14 +74,14 @@ if [ $? = 0 ]; then
 	nmcli radio wifi off
 	rfkill unblock wlan
 	#cat $pwd/$cert/$cert.conf
-	hostapd-wpe $pwd/$cert/$cert.conf
-	if [ $? = 0 ]; then
-		echo '\e[36mSistema levantado correctamente\e[39m'
-		echo
-	else
-		echo '\e[31mError ejecutando: hostapd-wpe $pwd/$cert/$cert.conf -s , intentarlo manualmente.\e[39m'
-		echo
-	fi
+	hostapd-wpe $pwd/$cert/$cert.conf 2>&1 | tee -a $pwd/hostapd-wpe.log
+	#if [ $? = 0 ]; then
+	#	echo -e '\e[36mSistema levantado correctamente\e[39m'
+	#	echo
+	#else
+	#	echo -e '\e[31mError ejecutando: hostapd-wpe $pwd/$cert/$cert.conf -s , intentarlo manualmente.\e[39m'
+	#	echo
+	#fi
 else
 	echo 'Error ejecutando script apd_launchpad.py'
 	echo
