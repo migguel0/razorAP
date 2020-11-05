652cc9f5482df1e74077dd780334083359c87a41
migguel04 <43185771+migguel0@users.noreply.github.com>
2020-11-05 20:16:25 +0100

    Update razorAP.sh

--- installer/razorAP.sh
+++ installer/razorAP.sh
@@ -13,34 +13,31 @@ echo
 
 pwd=$(pwd)
 
-	#echo "Error: Primer parametro del script NOMBRE_CERTIFICADO, segundo parametro SSID y tercer parametro interfaz a usar y cuarto parametro CommonName."
-	#echo "Ejemplo: ./runHostAPD.sh DEMOBANK CORPORATE-WIFI wlan0 COPORATE.DEMOBANK.COM"
-
-echo -e "\e[32mSeleccione la interfaz a usar\e[39m" 'Ex: wlan0'
+echo -e "\e[32mSelect the interface to be used\e[39m" 'Ex: wlan0'
 read interface
 
-echo -e "\e[32mEscriba el SSID del AP\e[39m"
+echo -e "\e[32mInsert AP SSID\e[39m"
 read SSID
 
-echo -e "\e[32mEscriba el nombre del certificado a incluir en el AP\e[39m"
+echo -e "\e[32mWrite the name of the certificate to be created\e[39m"
 read cert
 
-echo -e "\e[32mEscriba el CommonName\e[39m" 'Ex: COPORATE.DEMOBANK.COM'
+echo -e "\e[32mWrite the CommonName\e[39m" 'Ex: COPORATE.DEMOBANK.COM'
 read commonName
 
-echo -e "\e[32m¿Se va a desplegar en banda de 2.4GHz? \e[39m" '[si] [no]'
+echo -e "\e[32mIt will be deployed in the 2.4GHz band? \e[39m" '[yes] [no]'
 read band
 
-YES="si"
+YES="yes"
 
 if [ $band = $YES ]; then
-        echo -e "\e[32mSeleccione un canal entre 1 y 13 \e[39m"
+        echo -e "\e[32mSelect a channel between 1 and 13 \e[39m"
         read channel
 	if [ $channel -lt 1 ] || [ $channel -gt 13 ]; then
 		exit 1
 	fi
 else
-        echo -e "\e[32mSeleccione un canal entre los rango 34-64 y 100-140 \e[39m"
+        echo -e "\e[32mSelect a channel between the ranges 34-64 and 100-140 \e[39m"
         read channel
 	if [ $channel -lt 34 ] || [ $channel -gt 140 ]; then
 		exit 1
@@ -50,13 +47,12 @@ else
 fi
 
 
-echo -e "\e[33mElaborando certificados y archivos de configuracion... \e[39m"
+echo -e "\e[33mCreating certificates and configuration files... \e[39m"
 python $pwd/hostapdWPEinstall/apd_launchpad/apd_launchpad.py -t $cert -s $SSID -i $interface -cn $commonName
 
 if [ $? = 0 ]; then
-	echo -e "\e[33mTodo generado correctamente \e[39m"
-	echo -e "\e[33mLevantando hosatpd-wpe \e[39m"
-	echo -e "\e[33mSe va a ejecutar: hostapd-wpe $pwd/$cert/$cert.conf -s \e[39m"
+	echo -e "\e[33mEverything correctly generated \e[39m"
+	echo -e "\e[33mDeploying AP \e[39m"
 	echo
 
 	echo "" >> $pwd/$cert/$cert.conf
@@ -75,14 +71,8 @@ if [ $? = 0 ]; then
 	rfkill unblock wlan
 	#cat $pwd/$cert/$cert.conf
 	hostapd-wpe $pwd/$cert/$cert.conf 2>&1 | tee -a $pwd/hostapd-wpe.log
-	#if [ $? = 0 ]; then
-	#	echo -e '\e[36mSistema levantado correctamente\e[39m'
-	#	echo
-	#else
-	#	echo -e '\e[31mError ejecutando: hostapd-wpe $pwd/$cert/$cert.conf -s , intentarlo manualmente.\e[39m'
-	#	echo
-	#fi
+	
 else
-	echo 'Error ejecutando script apd_launchpad.py'
+	echo 'Error running script apd_launchpad.py'
 	echo
 fi
